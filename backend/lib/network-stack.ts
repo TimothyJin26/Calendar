11039287127859c078e14a15870dabfa76475ca9
Timothy Jin <Timothy.jin26@gmail.com>
2025-10-09 14:47:07 -0700

    Updated config to new dev deployment and revert amplify version to fix codegen

--- backend/lib/network-stack.ts
+++ backend/lib/network-stack.ts
@@ -30,11 +30,6 @@ export class NetworkStack extends cdk.Stack {
         },
         {
           cidrMask: 24,
-          name: 'private',
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED, // Keep for compatibility, no NAT
-        },
-        {
-          cidrMask: 24,
           name: 'isolated',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
